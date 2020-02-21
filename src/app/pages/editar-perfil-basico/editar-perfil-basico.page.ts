74dabb2bd7c5388b9e1cf7fa77e19e5caf6bcdb5
Efrain Ake <efrain.ake97@gmail.com>
2020-02-21 13:31:43 -0600

    commit 20 editar perfil basico

--- src/app/pages/editar-perfil-basico/editar-perfil-basico.page.ts
+++ src/app/pages/editar-perfil-basico/editar-perfil-basico.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Candidate, User } from '../../interfaces/interfaces';
+import { CandidateService } from '../../services/candidate.service';
+import { UsersService } from '../../services/users.service';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-editar-perfil-basico',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarPerfilBasicoPage implements OnInit {
 
-  constructor() { }
+  candidate: Candidate = {};
+  user: User = {};
+  constructor(private candidateService: CandidateService, private userService: UsersService,
+              private storage: Storage) {
+
+  }
 
   ngOnInit() {
+    this.storage.get('id').then((val) => {
+      this.candidateService.getCandidate(val).subscribe(candidato => {
+        this.candidate = candidato;
+        console.log(this.candidate);
+        });
+      this.userService.getUser(val).subscribe(user => {
+        this.user = user;
+        console.log(this.user);
+      });
+    });
   }
 
 }
